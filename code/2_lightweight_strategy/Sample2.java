91c729a638698bccf4bb5f941a60fe66923a5db8
thaint <thai@emobi.biz>
2022-05-16 16:10:12 +0700

    Bad codes

--- code/2_lightweight_strategy/Sample2.java
+++ code/2_lightweight_strategy/Sample2.java
@@ -10,9 +10,27 @@ public class Sample2 {
     return total;
   }
 
+  private static int totalEvenValues(List<Integer> numbers) {
+    int total = 0;
+    for (Integer number : numbers) {
+      if (number % 2 == 0) total += number;
+    }
+    return total;
+  }
+
+  private static int totalOddValues(List<Integer> numbers) {
+    int total = 0;
+    for (Integer number : numbers) {
+      if (number % 2 != 0) total += number;
+    }
+    return total;
+  }
+
   public static void main(String[] args) {
     List<Integer> values = Arrays.asList(1, 2, 3, 4, 5, 6);
 
     System.out.println(totalValues(values));
+    System.out.println(totalEvenValues(values));
+    System.out.println(totalOddValues(values));
   }
 }
